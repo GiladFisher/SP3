e0accceea707dd7846e2d87043032d08cdaefd45
GiladFisher <giladfisher98@gmail.com>
2022-12-22 20:19:33 +0200

    Set up input in main

--- main.c
+++ main.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
+#define N 50
 void shift_element(int* arr, int i);
 void insertion_sort(int* arr, int len);
 int main(){
-    int arr[9] = {7,3,8,5,10,9,1,0,11};
-    insertion_sort(arr, 9);
-    // shift_element(arr + 3, 5-3);
-    for (int i = 0; i < 9; i++)
+    int arr[N];
+    for (int i = 0; i < N; i++)
     {
-        printf("%d ,", *(arr + i));
+        scanf("%d", (arr + i));
     }
-    printf("\n");
-    
+    insertion_sort(arr, N);
+    for (int i = 0; i < N; i++)
+    {
+        if (i == N - 1){
+            printf("%d", *(arr + i));
+        }
+        else{
+            printf("%d,", *(arr + i));
+        }
+        
+    }
     return 0;
 }
