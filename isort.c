f9f1adbde7fbd3ff0ab9f35d69626b61e5dea97f
GiladFisher <giladfisher98@gmail.com>
2022-12-22 17:50:04 +0200

    Finished isort

--- isort.c
+++ isort.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-void shift_element(int* arr, int i){
+void shift_element(int *arr, int i){
     int temp1 = *(arr);
     int temp2 = *(arr + 1);
     int j = 0;
@@ -8,5 +8,29 @@ void shift_element(int* arr, int i){
         temp1 = temp2;
         temp2 = *(arr + j + 2); // arr[j + 2]
         j++;
+    }
+}
+void insertion_sort(int* arr, int len){
+    for (int i = 1; i < len; i++){
+        if (*(arr + i) < *(arr + i - 1)){
+            int temp = *(arr + i);
+            if ( i == 1){
+                shift_element(arr, 1);
+                *arr = temp;
+            }
+            else{
+                for (int j = i - 2; j >= 0; j--){
+                if (temp > *(arr + j)){
+                    shift_element((arr + j), i - j);
+                    *(arr + j + 1) = temp;
+                    break;
+                }
+                if (j == 0){
+                    shift_element(arr, i);
+                    *(arr) = temp;
+                }
+            }
+            }
+        }
     }
 }
--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
+void shift_element(int* arr, int i);
+void insertion_sort(int* arr, int len);
 int main(){
     int arr[9] = {7,3,8,5,10,9,1,0,11};
-    shift_element(arr, 3);
+    insertion_sort(arr, 9);
+    // shift_element(arr + 3, 5-3);
     for (int i = 0; i < 9; i++)
     {
         printf("%d ,", *(arr + i));
