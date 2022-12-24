e9c9dffe351e6ea4854fe1d080f1b2164f396ef1
GiladFisher <giladfisher98@gmail.com>
2022-12-24 16:02:48 +0200

    Wrote similar method

--- txtfind.c
+++ txtfind.c
@@ -16,11 +16,49 @@ int getword(char w[]){
     return len;
 }
 
+// the next method will return 1 if it is possible to get str1 by removing at most one char from str2
+int similar(char *str1, char *str2){
+    int wrng_chrs = 0; // will contain the number of chars not consistant with str1
+    char *p1 = str1;
+    char *p2 = str2;
+    while(1){
+        if (!*p1){
+            if(*p2){
+                p2 = (p2 + 1);
+                wrng_chrs++;
+            }
+            else{
+                break;
+            }
+        }
+        else if(!*p2){
+            return 0;
+        }
+        else if (*p1 != *p2){
+            wrng_chrs++;
+            p2 = (p2 + 1);
+        }
+        else{
+            p2 = (p2 + 1);
+            p1 = (p1 + 1);
+        }
+    }
+    if(wrng_chrs > 1){
+        return 0;
+    }
+    return 1;
+}
+
 int main(){
     char st[WORD];
+    char st2[WORD];
     scanf("%s",st);
-    int w_len = 0;
-    w_len = getword(st);
-    printf("%d\n",w_len);
+    scanf("%s",st2);
+
+    // int w_len = 0;
+    // w_len = getword(st);
+    int ans = -1;
+    ans = similar(st, st2);
+    printf("%d\n",ans);
     return 0;
 }
